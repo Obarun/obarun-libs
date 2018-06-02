5227482ec203c4c31afda4b05fad8a4926ab7930
obarun <eric@obarun.org>
2018-06-02 12:29:44 +1100

    mount_umount : check every directory

--- lib/filesystem.sh
+++ lib/filesystem.sh
@@ -53,28 +53,68 @@ mount_umount(){
 		out_action "Check mounted filesystem on $rep"
 		if ! [[ $(mount | grep "$rep"/proc) ]]; then
 			_mount -t proc proc "$rep/proc" -o nosuid,noexec,nodev 
+		else
+			out_valid "$rep/proc already mounted"
+		fi
+		if ! [[ $(mount | grep "$rep"/sys) ]]; then
 			_mount -t sysfs sys "$rep/sys" -o nosuid,noexec,nodev,ro 
+		else
+			out_valid "$rep/sys already mounted"
+		fi
+		if ! [[ $(mount | grep "$rep"/dev) ]]; then
 			_mount -t devtmpfs dev "$rep/dev" -o mode=0755,nosuid
 			_mount -t devpts devpts "$rep/dev/pts" -o mode=0620,gid=5,nosuid,noexec
 			_mount -t tmpfs shm "$rep/dev/shm" -o mode=1777,nosuid,nodev
+		else
+			out_valid "$rep/dev already mounted"
+		fi
+		if ! [[ $(mount | grep "$rep"/run) ]]; then
 			_mount -t tmpfs run "$rep/run" -o nosuid,nodev,mode=0755
+		else
+			out_valid "$rep/run already mounted"
+		fi
+		if ! [[ $(mount | grep "$rep"/tmp) ]]; then
 			_mount -t tmpfs tmp "$rep/tmp" -o mode=1777,strictatime,nodev,nosuid
 		else
-			out_valid "Filesystem already mounted in ${rep}"
+			out_valid "$rep/tmp already mounted"
 		fi
 	fi
 	if [[ "$action" == "umount" ]]; then
-		out_action "Check mounted filesystem"
+		out_action "Check mounted filesystem on $rep"
 		if [[ $(mount | grep "$rep"/proc) ]]; then
 			_umount "$rep/proc"
+		else
+			out_valid "$rep/proc not mounted"
+		fi
+		if [[ $(mount | grep "$rep"/sys) ]]; then
 			_umount "$rep/sys"
+		else
+			out_valid "$rep/sys not mounted"
+		fi
+		if [[ $(mount | grep "$rep"/dev/pts) ]]; then
 			_umount "$rep/dev/pts"
+		else
+			out_valid "$rep/dev/pts not mounted"
+		fi
+		if [[ $(mount | grep "$rep"/dev/shm) ]]; then
 			_umount "$rep/dev/shm"
+		else
+			out_valid "$rep/dev/shm not mounted"
+		fi
+		if [[ $(mount | grep "$rep"/run) ]]; then
 			_umount "$rep/run"
+		else
+			out_valid "$rep/run not mounted"
+		fi
+		if [[ $(mount | grep "$rep"/tmp) ]]; then
 			_umount "$rep/tmp"
+		else
+			out_valid "$rep/tmp not mounted"
+		fi
+		if [[ $(mount | grep "$rep"/dev) ]]; then	
 			_umount "$rep/dev"
 		else
-			out_valid "Filesystem not mounted in ${rep}"
+			out_valid "$rep/dev not mounted"
 		fi
 	fi
 	unset rep action
